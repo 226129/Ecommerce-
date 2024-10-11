db3c2472e66de144979dbb15b388756fe972ceba
226129 <al226129@alumnos.uacj.mx>
2024-10-11 17:02:40 -0600

    Added the store view with all the queries for the products display on the webpage

--- Ecommerce_project/Ecommerce_project/settings.py
+++ Ecommerce_project/Ecommerce_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'category.apps.CategoryConfig',
+    'store.apps.StoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
